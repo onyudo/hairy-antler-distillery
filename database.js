a67844742f42afe53c56d974d345a07083dc1545
“Martin <martin@forceandform.com>
2025-02-26 13:22:31 -0500

    Updated image file paths in database.js file.

--- database.js
+++ database.js
@@ -28,10 +28,10 @@ const insert = db.prepare(`
 `);
 
 // Still needs links to bottle images and longer product descriptions for each product here!
-insert.run('./img/hairy-antler-vodka.png', 'Kentucky Original Sorghum Vodka', 'Our 100% Estate grown Kentucky original Sorghum Vodka', 'longer prodcut description goes here', 35.99, 750, true);
-insert.run('./img/hairy-antler-gin.png', 'Kentucky Foraged Off Trail Gin', 'Kentucky foraged botanical Gin', 'longer prodcut description goes here', 35.99, 750, true);
-insert.run('./img/hairy-antler-demon-leaper.png', 'Demon Leaper Herbal Liqueur', 'Our limited edition Kentucky foraged Herbal Liqueur, aged for 5 years', 'longer prodcut description goes here', 65.99, 450, true);
-insert.run('./img/hairy-antler-rye.png', 'Kentucky Straight Rye Whiskey', 'Our 100% Kentucky Rye Whiskey, aged for 3 years', 'longer prodcut description goes here', 75.99, 750, true);
-insert.run('./img/hairy-antler-bourbon.png', 'Kentucky Straight Bourbon Whiskey', 'Our Kentucky Straight Bourbon Whiskey, aged for 7 years', 'longer prodcut description goes here', 85.99, 750, true);
+insert.run('../img/hairy-antler-vodka.png', 'Kentucky Original Sorghum Vodka', 'Our 100% Estate grown Kentucky original Sorghum Vodka', 'longer prodcut description goes here', 35.99, 750, true);
+insert.run('../img/hairy-antler-gin.png', 'Kentucky Foraged Off Trail Gin', 'Kentucky foraged botanical Gin', 'longer prodcut description goes here', 35.99, 750, true);
+insert.run('../img/hairy-antler-demon-leaper.png', 'Demon Leaper Herbal Liqueur', 'Our limited edition Kentucky foraged Herbal Liqueur, aged for 5 years', 'longer prodcut description goes here', 65.99, 450, true);
+insert.run('../img/hairy-antler-rye.png', 'Kentucky Straight Rye Whiskey', 'Our 100% Kentucky Rye Whiskey, aged for 3 years', 'longer prodcut description goes here', 75.99, 750, true);
+insert.run('../img/hairy-antler-bourbon.png', 'Kentucky Straight Bourbon Whiskey', 'Our Kentucky Straight Bourbon Whiskey, aged for 7 years', 'longer prodcut description goes here', 85.99, 750, true);
 
 console.log('Sample data inserted!');

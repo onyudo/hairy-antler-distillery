872d60a69ae3a2f097c102c1e864767168b9a4de
“Martin <martin@forceandform.com>
2025-03-13 17:13:19 -0400

    Additional updates to information in products.db and related updates in related files.

--- public/js/index.js
+++ public/js/index.js
@@ -24,8 +24,7 @@ if (productsList) {
                         <div class="product-atts">
                         <h2 class="product-descriptor">${product.product_descriptor}</h2>
                         <h2 class="product-name">${product.product_name}</h2>
-                        <p class="product-blurb">${product.product_blurb}</p>
-                        <a href="spirit.html?id=${product.product_id}">View More</a>
+                        <p class="product-blurb">${product.product_blurb}<br><a href="spirit.html?id=${product.product_id}" class="view-more">View More »</a></p>
                         <p class="product-price">$${product.product_price}</p>
                         <button class="add-to-cart" data-id="${product.product_id}" data-name="${product.product_name}" data-price="${product.product_price}" data-img="${product.product_img}">Add to Cart</button>
                         </div>
